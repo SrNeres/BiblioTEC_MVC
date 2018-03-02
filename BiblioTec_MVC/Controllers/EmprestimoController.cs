37968e50923e9bae20e8d884df78a2a1373367ed
Neres <gustavoneresl@gmail.com>
2018-03-02 18:54:34 -0300

    Comitando Teste

---
+++ BiblioTec_MVC/Controllers/EmprestimoController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BiblioTec_MVC.Controllers
+{
+    public class EmprestimoController : Controller
+    {
+        // GET: Emprestimo
+        public ActionResult Index()
+        {
+            return View("EmprestimoIndex");
+        }
+    }
+}
